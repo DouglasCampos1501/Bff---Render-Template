f7a78d338cd71fafc8487305dbcbbbeb826eb84d
Douglas Campos <dougcampos1501@gmail.com>
2023-02-27 21:03:04 -0300

    More Screen for Template

--- template.py
+++ template.py
@@ -10,10 +10,22 @@ def hello(name):
 def say_hello():
     return render_template('say_hello.html')
 
-@app.route('/say_hello', method='post')
+@app.route('/say_hello', methods=['post'])
 def say_hello_post():
     name = request.form['text']
     return hello(name)
 
+@app.route('/dict')
+def dict():
+    my_dict = {'name':'banana', 'quantidade' : 10}
+    return render_template('dict.html', item = my_dict)
+
+@app.route('/list')
+def list():
+    my_list = [{'name':'banana', 'price':10},
+               {'name':'Omo', 'price':1},
+               {'name':'Nescau', 'price':2},
+               {'name':'picanha', 'price':20}]
+    return render_template('list.html', list=my_list)
 
 app.run(debug=True)
